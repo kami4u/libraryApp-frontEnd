526102e8c9892c32e377af0446d5f66a5bf68b09
Kamran Mujtaba <kamran@thefreshuk.com>
2017-11-16 16:53:28 +0000

    did some initial setups

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">
+          This is a library app
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
